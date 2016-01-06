b0ae0455d460a9767fe767af9e2836c4ddaf840d
pawel labno <pawel.labno@outlook.com>
2016-01-06 10:11:22 +0100

    migracje i kalendarz - rozpoczęcie

--- Inzynierka/Controllers/CalendarController.cs
+++ Inzynierka/Controllers/CalendarController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Inzynierka.Models;
+using Inzynierka.ViewModel.Calendar;
+using Microsoft.AspNet.Identity;
 
 namespace Inzynierka.Controllers
 {
@@ -11,6 +14,49 @@ namespace Inzynierka.Controllers
         // GET: Calendar
         [Authorize]
         public ActionResult Index()
+        {
+            var calendarItems = new List<CalendarItem>();
+
+            var calendarContext = new ScientificWorkContext();
+            string userId = User.Identity.GetUserId();
+            var plans = calendarContext.ActivityPlans.Where(o => o.UserId == userId).ToList();
+
+            foreach ( var plan in plans )
+            {
+                calendarItems.Add(new CalendarItem()
+                {
+                    Name = plan.ActivityName,
+                    Date = plan.Date,
+                    Id = plan.Id
+                });
+            }
+
+            ViewBag.Items = calendarItems;
+            return View();
+        }
+
+        // GET: Calendar/{0}
+        [Authorize]
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        // POST: Calendar
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Delete()
         {
             return View();
         }
---
+++ Inzynierka/Models/ActivityPlan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Inzynierka.Models
+{
+    [Table( "ActivityPlan" )]
+    public class ActivityPlan
+    {
+        public int Id
+        {
+            get; set;
+        }
+
+        [MinLength(5)]
+        [Column("Name")]
+        public string ActivityName
+        {
+            get; 
+            set;
+        }
+
+        public string UserId
+        {
+            get; 
+            set;
+        }
+        
+        public DateTime? Date 
+        { 
+            get; 
+            set; 
+        }
+    }
+}
