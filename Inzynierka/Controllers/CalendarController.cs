f330dc027696c3a9ebc8b084661139bec92cb54e
pawel labno <pawel.labno@outlook.com>
2016-01-06 14:33:26 +0100

    Kalendarz niemal cały

--- Inzynierka/Controllers/CalendarController.cs
+++ Inzynierka/Controllers/CalendarController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,21 +17,24 @@ namespace Inzynierka.Controllers
         public ActionResult Index()
         {
             var calendarContext = new ScientificWorkContext();
+
+            CalendarListViewModel listViewModel = new CalendarListViewModel();
             string userId = User.Identity.GetUserId();
             var plans = calendarContext.ActivityPlans.Where(o => o.UserId == userId).ToList();
 
             var calendarItems = plans.Select(plan => new CalendarItem()
             {
-                Name = plan.ActivityName, Date = plan.Date.Value.ToString( "dd MMMM yyyy, dddd" ), Id = plan.Id
+                Name = plan.ActivityName, Date = plan.Date.Value.ToString( "dd MMMM yyyy, dddd" ), Id = plan.Id, Type = (int) plan.Type
             }).ToList();
             var dateTimeNowString = DateTime.Now.Date.ToString( "dd MMMM yyyy, dddd" );
-            var todayItems = calendarItems.Where(o => o.Date == dateTimeNowString );
+            var todayItems = calendarItems.Where(o => o.Date == dateTimeNowString ).ToList();
 
-            ViewBag.HasTodayEvents = todayItems.Any();
-            ViewBag.TodayItems = todayItems;
+            listViewModel.HasTodayEvents = todayItems.Any();
+            listViewModel.TodayItems = todayItems;
 
             ViewBag.Items = calendarItems;
-            return View();
+
+            return View( listViewModel );
         }
         
         // GET: Calendar/Details/{0}
@@ -68,19 +72,110 @@ namespace Inzynierka.Controllers
         [Authorize]
         public ActionResult Create()
         {
-            return View();
+            var viewModel = new CalendarEntryEditViewModel();
+            viewModel.Date = DateTime.Now.Date.ToString( "dd-MMM-yyyy" );
+            viewModel.ActivityTypes = new List<SelectListItem>();
+            viewModel.Type = ActivityType.Deadline;
+            string userId = User.Identity.GetUserId();
+            viewModel.UserId = userId;
+
+            CreateSelectItemForActivityType( viewModel.ActivityTypes, Resources.Strings.Resource.PublicationText, "1" );
+            CreateSelectItemForActivityType( viewModel.ActivityTypes, Resources.Strings.Resource.DeadlineText, "2" );
+            CreateSelectItemForActivityType( viewModel.ActivityTypes, Resources.Strings.Resource.ReviewText, "3" );
+            CreateSelectItemForActivityType( viewModel.ActivityTypes, Resources.Strings.Resource.StartWritingText, "4" );
+
+            return View( viewModel );
         }
 
         [Authorize]
-        public ActionResult Edit(string parameter)
+        public ActionResult Edit()
         {
-            return View();
+            var calendarContext = new ScientificWorkContext();
+
+            var id = RouteData.Values.ContainsKey( "id" ) ? RouteData.Values["id"].ToString() : null;
+            if ( String.IsNullOrEmpty( id ) )
+            {
+                return RedirectToActionPermanent( "ActivityError" );
+            }
+
+            var data = int.Parse( id );
+
+            var plan = calendarContext.ActivityPlans.FirstOrDefault( o => o.Id == data );
+
+            if ( plan == null )
+            {
+                return RedirectToActionPermanent( "ActivityError" );
+            }
+
+            var viewModel = new CalendarEntryEditViewModel();
+            viewModel.ActivityName = plan.ActivityName;
+            viewModel.Date = plan.Date.Value.ToString( "dd-MMM-yyyy" );
+            viewModel.ActivityTypes = new List<SelectListItem>();
+            viewModel.Id = plan.Id;
+            viewModel.Type = plan.Type;
+            viewModel.Description = plan.Description;
+            viewModel.UserId = plan.UserId;
+            
+            CreateSelectItemForActivityType( viewModel.ActivityTypes , Resources.Strings.Resource.PublicationText, "1" );
+            CreateSelectItemForActivityType( viewModel.ActivityTypes, Resources.Strings.Resource.DeadlineText, "2" );
+            CreateSelectItemForActivityType( viewModel.ActivityTypes, Resources.Strings.Resource.ReviewText, "3" );
+            CreateSelectItemForActivityType( viewModel.ActivityTypes, Resources.Strings.Resource.StartWritingText, "4" );
+           
+            return View( viewModel );
+        }
+
+        private void CreateSelectItemForActivityType( IList<SelectListItem> elements, string caption, string value )
+        {
+            var  newItem = new SelectListItem();
+            newItem.Value = value;
+            newItem.Text = caption;
+            elements.Add(newItem);
         }
 
         [Authorize]
         public ActionResult Delete(string parameter)
         {
             return View();
+        }
+
+        [Authorize]
+        public ActionResult SaveActivity()
+        {
+            var request = Request.Form;
+            var calendarContext = new ScientificWorkContext();
+
+            var id = request.GetValues("id");
+
+            ActivityPlan itemToSave = null;
+
+            if ( id != null )
+            {
+                var intId = int.Parse( id[0] );
+                itemToSave = calendarContext.ActivityPlans.FirstOrDefault( o => o.Id == intId );
+            }
+
+            if ( itemToSave == null )
+            {
+                itemToSave = new ActivityPlan();
+            }
+
+            itemToSave.Description = request.Get("Description");
+            itemToSave.Date = DateTime.Parse(request.Get("Date"));
+            itemToSave.Type = (ActivityType) Enum.Parse(typeof(ActivityType), request.Get("Type"));
+            itemToSave.ActivityName = request.Get("ActivityName");
+            itemToSave.UserId = request.Get("UserId");
+
+            if ( itemToSave.Id == 0 )
+            {
+                calendarContext.ActivityPlans.Add( itemToSave );
+            }
+            else
+            {
+                calendarContext.ActivityPlans.AddOrUpdate( itemToSave );
+            }
+            calendarContext.SaveChanges();
+
+            return RedirectToActionPermanent( "Index" );
         }
     }
 }
---
+++ Inzynierka/ViewModel/Calendar/CalendarListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inzynierka.ViewModel.Calendar
+{
+    public class CalendarListViewModel
+    {
+        public bool HasTodayEvents { get; set; }
+
+        public IEnumerable<CalendarItem> TodayItems { get; set; }
+    }
+}
